c9b930637b56ac3e6d509ddce6438833d2b2dea4
YvesB. <migjess@yahoo.de>
2025-01-02 18:52:51 +0100

    Erweiterung
    Knopf

--- Main.py
+++ Main.py
@@ -4,15 +4,21 @@ import tkinter as tk
 fensterBreite=450
 fensterHoehe=450
 fensterHintergrundFarbe="#FFC7EF"
-fensterSchriftFarbe="#000000"
+schriftFarben="#000000"
 fensterTitel="ProjektA"
+knopfFarbe="#CB0794"
+knopfBreite=130
+knopfHoehe=50
+
 
 # Fenstererstellung(init)
 fenster = tk.Tk()
 fenster.geometry(f"{fensterBreite}x{fensterHoehe}")
 fenster.title(fensterTitel)
 fenster.configure(background=fensterHintergrundFarbe)
+
+# FensterInhalt(widgets)
+knopf = tk.Button(fenster)
+knopf.configure(background=knopfFarbe, text="Touch Me", foreground=schriftFarben)
+knopf.place(width=knopfBreite,height=knopfHoehe,x=fensterBreite/2, y=fensterHoehe/2,anchor="center")
 fenster.mainloop()
-
-
-
